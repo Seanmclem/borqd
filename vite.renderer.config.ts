c097f845d2b0fc32aa5a8f997f6fbd3f7c71ccb2
Seanmclem <seanmclements@gmail.com>
2025-07-19 23:17:09 -0400

    vibe add tailwind

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  // Vite will automatically detect postcss.config.js, so we don't need to specify it explicitly
+});
